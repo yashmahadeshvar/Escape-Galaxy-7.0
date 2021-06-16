eae41764a4f66f94026d41c1a5fb641159a3261d
yashmahadeshvar <75307261+yashmahadeshvar@users.noreply.github.com>
2021-06-16 14:55:19 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,9 +1,7 @@
 class Game{
-        constructor(){
-
-        }
+    constructor(){}
       
-      start(){
+    start(){
         bg = createSprite(200,300,400,800);
         bg.addImage(backgroundIMG);
         bg.scale = 0.45;
@@ -19,142 +17,138 @@ class Game{
         info = createSprite(81,572,10,10);
         info.addImage(infoIMG);
         info.scale = 0.08;        
+    }
 
-       
-
-      }
-
-      setLevel1(){
+    setLevel1(){
         bg2 = createSprite(200,300,400,625);
         bg2.addImage(backgroundIMG2);
         bg2.scale = 0.9;
         bg2.visible = false;
-       // bg2.velocityY = 2;
 
         astroJet = createSprite(200,580,10,10);
         astroJet.addImage(astroJetIMG);
         astroJet.visible=false;
         astroJet.scale = 0.4;
 
-        helth1 = createSprite(310,25,10,10);
-        helth1.addImage(helth1IMG);
-        helth1.scale = 0.3;
-        helth1.visible = false ;
+    }
 
-        helth2 = createSprite(310,25,10,10);
-        helth2.addImage(helth2IMG);
-        helth2.scale = 0.4;
-        helth2.visible = false ;
-
-      }
-
-      playLevel1(){
+    playLevel1(){
 
         textSize(18);
         text("level 1",170,25); 
         textSize(15);   
-        text("Target = 1500",260,60);   
+        text("Target = 500",300,60);   
         
         bg2.visible = true;
         astroJet.visible = true;
-        helth1.visible = true;
-        helth2.visible = false;
         title.visible = false;
 
+        //spawning aliens
         if (frameCount % 90=== 0) {
           var alien = createSprite(100,70,50,50);
           alien.x = Math.round(random(50,350));
           alien.addImage(alienIMG);
-          alien.scale = 0.25;
+          alien.scale = 0.2;
           alien.velocityY = 10;
           alien.lifetime = 200;
           alienGroup.add(alien);
-       }
+        }
 
+        //spawning meteors
+        if (frameCount % 80=== 0) {
+          var meteor = createSprite(100,70,50,50);
+          meteor.x = Math.round(random(50,350));
+          meteor.addImage(meteorIMG);
+          meteor.scale = 0.05;
+          meteor.velocityY = 8;
+          meteor.lifetime = 200;
+          meteorGroup.add(meteor);
+        }
 
-      if(keyDown("space")){
-       var fireBall= createSprite(200,500,20, 20);
-       fireBall.addImage(fireBallIMG);
-       fireBall.x=astroJet.x;
-       fireBall.velocityY = -8 ;
-       fireBall.lifetime = 800;
-       fireBall.scale = 0.1;
-       fireBallGroup.add(fireBall);
+        //shooting fireball when space is pressed
+        if(keyWentDown("space")){
+          var fireBall= createSprite(200,500,20, 20);
+          fireBall.addImage(fireBallIMG);
+          fireBall.x=astroJet.x;
+          fireBall.velocityY = -8 ;
+          fireBall.lifetime = 800;
+          fireBall.scale = 0.1;
+          fireBallGroup.add(fireBall);
+        }
 
-  }
-       if(fireBallGroup.isTouching(alienGroup)){
-         alienGroup.destroyEach();
-         fireBallGroup.destroyEach();
-         score = score + 100
-       }
-       if(astroJet.isTouching(xpGroup)){
-        xpGroup.destroyEach();
-        xp1 = xp1 + 10
+        //destroying aliens by fireball
+        for(var fb = 0; fb < fireBallGroup.length; fb++){
+          for(var al=0;al<alienGroup.length;al++){
+            if(fireBallGroup.isTouching(alienGroup)){
+              alienGroup.get(al).remove();
+              fireBallGroup.get(fb).lifetime=0;
+              score = score + 100;
+            }
+          }
+        }
+        
+      //create boost
+      if (frameCount % 200 === 0) {
+        boost = createSprite(100,-50,50,50);
+        boost.x = Math.round(random(50,350));
+        boost.addImage(boostIMG);
+        boost.scale = 0.05;
+        boost.velocityY = 8;
+        boost.lifetime = 200;
+        boostGroup.add(boost);
       }
-      if(astroJet.isTouching(meteorGroup)){
-        helth1.visible = false ;
-        helth2.visible = true
+
+      //  if astrojet collects boost then lives increase
+      for(var b=0;b<boostGroup.length;b++){
+        if(boostGroup.isTouching(astroJet)){
+          boostGroup.get(b).remove();
+          lives = lives + 1;
+        }
       }
 
+      //lives reduces if meteor is touched
+      for(var m=0;m<meteorGroup.length;m++){
+        if(meteorGroup.isTouching(astroJet)){
+          meteorGroup.get(m).remove();
+          lives = lives - 1;
+        }
+      }
+  
+      //move to level 2 if score is 1500
       if(score === 500){
         gameState = 2;
       }
-      
-       if(xp1 === 200){
-        if(keyDown("space")){
-          var laser2= createSprite(200,500,20, 20);
-          laser2.addImage(laser2IMG);
-          laser2.x=astroJet.x;
-          laser2.velocityY = -8 ;
-          laser2.scale = 0.1;
-          laser2Group.add(laser2);
-      }
-       }
-     
 
-    
-       
-       //if (the fireball touches the alien){ it gets destroyed and score increases by 100}
-      
-
-       //create boost after every 200 frames
-       
-       //if (astrojet touches boost){ get bigger missile}
-
-       //create meteors after every 150 frames
-
-       //if (astrojet touches meteor or alien){ then lives reduce by 1}
-
-       //if (score is 1000){ then gameState changes to 2}
-
-       //if (lives is 0){ then go back to gameState 0}
-
-      }
-
+    }
 
     playLevel2(){
+
       textSize(18);
-      text("level 2",200,25); 
+      text("level 2",180,25); 
       textSize(15);   
-      text("Target = 3000",260,60);  
+      text("Target = 1000",260,60);  
+
+      //spawning ufo with laser
         if (frameCount % 100=== 0) {
           var ufo = createSprite(100,70,50,50);
           ufo.x = Math.round(random(50,350));
           ufo.addImage(ufoIMG);
-          ufo.scale = 0.50;
+          ufo.scale = 0.35;
           ufo.velocityY = 9;
           ufo.lifetime = 800;
           ufoGroup.add(ufo);
-       }
-       if (frameCount % 40=== 0) {
-        var laser1 = createSprite(100,70,50,50);
-          laser1.x = Math.round(random(50,350));
+
+          var laser1 = createSprite(100,70,50,50);
+          laser1.x = ufo.x;
           laser1.addImage(laser1IMG);
           laser1.scale = 0.1;
           laser1.velocityY = 20;
           laser1.lifetime = 800;
-          laser1Group.add(laser1)
-     }
+          laser1Group.add(laser1);
+
+       }
+
+       //shooting missiles on space key
        if(keyDown("space")){
         var missile= createSprite(200,500,20, 20);
         missile.addImage(missileIMG);
@@ -163,71 +157,99 @@ class Game{
         missile.lifetime = 800;
         missile.scale = 0.3;
         missileGroup.add(missile);
-    }
-      if(astroJet.isTouching(meteorGroup)){
-        helth1.visible = false ;
-        helth2.visible = true
       }
 
+      //destroying ufo and laser
       if(missileGroup.isTouching(ufoGroup)){
         ufoGroup.destroyEach();
         missileGroup.destroyEach();
-        score = score + 100
+        laser1Group.destroyEach();
+        score = score + 100;
       }
+
+      //move to level 3 when score is 1000
       if(score === 1000){
         gameState = 3;
       }
+
     }
 
-  playLevel3(){
+    playLevel3(){
 
-    textSize(15);
-    text("END LEVEL",200,25);   
-    textSize(15);   
-    text("Target = 5000",260,60);
+      textSize(15);
+      text("END LEVEL",180,25);   
+      textSize(15);   
+      text("Target = 15-Hits",260,60);
+      textSize(13);
+      text("BOSS-HEALTH :"+bossHealth,10,25);
 
-    boss = createSprite(200,160,10,10);
-    boss.addImage(bossIMG);
-    boss.scale = 0.9;
+      boss = createSprite(200,160,10,10);
+      boss.addImage(bossIMG);
+      boss.scale = 0.9;
 
-    if (frameCount % 10 === 0) {
-      var laser3 = createSprite(100,150,50,50);
-        laser3.x = Math.round(random(50,350));
-        laser3.addImage(laser3IMG);
-        laser3.scale = 0.1;
-        laser3.velocityY = 20;
-        laser3.lifetime = 800;
-        laser3Group.add(laser3)
-   }
- if (frameCount % 5 === 0) {
-   var laser4 = createSprite(100,70,50,50);
-     laser4.x = Math.round(random(10,50));
-     laser4.addImage(laser4IMG);
-     laser4.scale = 0.1;
-     laser4.velocityY = 20;
-     laser4.lifetime = 800;
-     laser4Group.add(laser4)
- }
- if (frameCount % 5 === 0) {
-  var laser5 = createSprite(100,70,50,50);
-    laser5.x = Math.round(random(350,390));
-    laser5.addImage(laser5IMG);
-    laser5.scale = 0.1;
-    laser5.velocityY = 20;
-    laser5.lifetime = 800;
-    laser5Group.add(laser5)
- }
- if(keyDown("space")){
-  var trident= createSprite(200,500,20, 20);
-  trident.addImage(tridentIMG);
-  trident.x=astroJet.x;
-  trident.velocityY = -10;
-  trident.lifetime = 800;
-  trident.scale = 0.3;
-  tridentGroup.add(trident);
- }
-  if(score === 5000){
+      //blue laser
+      if (frameCount % 30 === 0) {
+        var laser3 = createSprite(100,150,50,50);
+          laser3.x = Math.round(random(50,350));
+          laser3.addImage(laserIMG);
+          laser3.scale = 0.1;
+          laser3.velocityY = 20;
+          laser3.lifetime = 800;
+          laser3Group.add(laser3)
+      }
+
+      //left red laser
+        if (frameCount % 30 === 0) {
+          var laser4 = createSprite(100,70,50,50);
+            laser4.x = Math.round(random(10,50));
+            laser4.addImage(laser1IMG);
+            laser4.scale = 0.1;
+            laser4.velocityY = 20;
+            laser4.lifetime = 800;
+            laser4Group.add(laser4)
+        }
+
+      //right red laser  
+        if (frameCount % 30 === 0) {
+          var laser5 = createSprite(100,70,50,50);
+            laser5.x = Math.round(random(350,390));
+            laser5.addImage(laser1IMG);
+            laser5.scale = 0.1;
+            laser5.velocityY = 20;
+            laser5.lifetime = 800;
+            laser5Group.add(laser5)
+        }
+
+        //shooting trident
+        if(keyWentDown("space")){
+          var trident= createSprite(200,500,20, 20);
+          trident.addImage(tridentIMG);
+          trident.x=astroJet.x;
+          trident.velocityY = -10;
+          trident.lifetime = 800;
+          trident.scale = 0.4;
+          tridentGroup.add(trident);
+        }
+
+        //boss health to reduce by 1
+        for(var t=0; t<tridentGroup.length; t++){
+          if(tridentGroup.isTouching(boss)){
+            tridentGroup.get(t).remove();
+            bossHealth = bossHealth - 1;
+          }
+        }
+        
+        //when boss dies 
+          if(bossHealth === 0){
+            boss.visible=false;
+            gameState=4;
+          }
+      }
+
+  
+  end(){
+  //code for end look
   }
- }
 
 }
+
--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,6 @@
-var canvas;
 var gameState=0;
-var form,game;
+var game;
 var astroJet,astroJetIMG;
-var xp,xpIMG,xpGroup;
 var meteor,meteorIMG,meteorGroup;
 var alien,ufo,boss,fireBall,fireBallIMG,fireBall2IMG;
 var alienGroup,ufoGroup,fireBallGroup2,fireBall2;
@@ -12,39 +10,34 @@ var out,outIMG;
 var missile,missileIMG,missileGroup;
 var trident,tridentIMG,tridentGroup;
 var startButton,startButtonIMG;
-var helth1,helth2,helth1IMG,helth2IMG;
 var title,titleIMG,info,infoIMG;
 var laser1,laser1IMG,laser1Group;
 var laser2,laser2IMG,laser2Group;
 var laser3,laser3IMG,laser3Group;
 var laser4,laser4IMG,laser4Group;
 var laser5,laser5IMG,laser5Group;
+var boost, boostIMG, boostGroup;
+var lives = 5;
 var score = 0;
-var xp1 = 0;
+var bossHealth = 15;
 var endIMG,end;
 
 function preload(){
   backgroundIMG = loadImage("images/bg.jpg");
-  backgroundIMG2 = loadImage("images/bg 1.jpg");
+  backgroundIMG2 = loadImage("images/bg1.jpg");
   alienIMG = loadImage("images/alien.png");
   astroJetIMG = loadImage("images/jet.png");
   ufoIMG = loadImage("images/UFO.png");
   startButtonIMG = loadImage("images/button.png");
   fireBallIMG = loadImage("images/fireBall.png");
-  fireBall2IMG = loadImage("images/laser.png");
   bossIMG = loadImage("images/boss.png");
-  xpIMG = loadImage("images/xp.png");
-  meteorIMG = loadImage("images/meteor 2.png");
-  helth1IMG = loadImage("images/helth1.png");
-  helth2IMG = loadImage("images/helth2.png");
+  meteorIMG = loadImage("images/meteor2.png");
+  boostIMG = loadImage("images/boost.png");
   titleIMG = loadImage("images/title.png");
   infoIMG = loadImage("images/info.png");
   missileIMG = loadImage("images/missile.png");
-  laser1IMG = loadImage("images/laser 1.png");
-  laser2IMG = loadImage("images/laser 1.png");
-  laser3IMG = loadImage("images/laser.png");
-  laser4IMG = loadImage("images/laser 1.png");
-  laser5IMG = loadImage("images/laser 1.png");
+  laserIMG = loadImage("images/laser.png");
+  laser1IMG = loadImage("images/laser1.png");
   tridentIMG = loadImage("images/trident.png");
 }
 
@@ -54,11 +47,13 @@ function setup() {
   game.start();
   game.setLevel1();
 
+  //Groups
   fireBallGroup=createGroup();
   alienGroup=createGroup();
-  xpGroup=createGroup();
   meteorGroup=createGroup();
   ufoGroup=createGroup();
+  boostGroup=createGroup();
+
   missileGroup=createGroup();
   laser1Group=createGroup();
   laser2Group=createGroup();
@@ -72,20 +67,17 @@ function setup() {
 function draw() {
   background(0);
   drawSprites();
-
+  astroJet.x = mouseX;
+  //display score & xp throughout the game
   fill("gold");
-        textSize(13);
-        text("score :"+score,10,25);
-        textSize(12);
-        text("XP :"+xp1,10,40);
-        textSize(15);
-        text("--------------------------------------------------------------------------------------",1,70);       
+  textSize(13);
+  text("score :"+score,10,25);
+  text("Lives: "+ lives,325,25);    
+  textSize(15);
+  text("--------------------------------------------------------------------------------------",1,70);       
+    
   
 
-
-        astroJet.x = mouseX;
-       
-            
   if(gameState === 0){
       fill("gold")
       textSize(15);
@@ -93,7 +85,7 @@ function draw() {
       textSize(15);
       text("INFO",110,576); 
       textSize(15);
-      text("--------------------------------------------------------------------------------------",1,130);       
+      text("--------------------------------------------------------------------------------------",1,120);   
 
     if(mousePressedOver(startButton)){
       gameState = 1;
@@ -101,44 +93,37 @@ function draw() {
       startButton.visible = false;
 
     }
-   
+
+    if(mousePressedOver(info)){
+      textSize(15);
+      text("Level 1 : Instructions \n1. Use mouse To move\n2. Destroy aliens to proceed further",160,555);
+    }
   }
 
   if(gameState === 1){
     game.playLevel1();
-    spawnMeteor();
   }
   if(gameState === 2){
     game.playLevel2();
-    spawnMeteor();
   }
   if(gameState === 3){
     game.playLevel3();
-    spawnMeteor();
   }
-  if(gameState === 4){
-    endLook();
-  }
-  if(mousePressedOver(info)){
-    textSize(15);
-    text("Level 1 : Instructions \n1. Use mouse To move\n2. Destroy aliens to proceed further",100,450);
-  }
-  
-     
-      
-}
 
-function spawnMeteor(){
-  if (frameCount % 30=== 0) {
-    var meteor = createSprite(100,70,50,50);
-    meteor.x = Math.round(random(50,350));
-    meteor.addImage(meteorIMG);
-    meteor.scale = 0.05;
-    meteor.velocityY = 8;
-    meteor.lifetime = 200;
-    meteorGroup.add(meteor);
+  //game ended
+  if(lives === 0){
+    text("Game Ended",200,300);
+    game.end();
   }
+
+  //when you finish all levels
+  if(gameState === 4){
+    //text("Congratulation");
+    game.end();
+  }
+
   
+        
 }
 
  
